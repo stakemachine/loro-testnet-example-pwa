83b1c10e91ccd098273a3277f81f24d3cabaedc9
Stake Machine <80471159+stake-machine@users.noreply.github.com>
2024-08-17 19:06:07 +0300

    Update publish-app-record.sh

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -24,7 +24,7 @@ services:
     rpcEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-https://laconicd.laconic.com}'
     gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-https://laconicd.laconic.com/api}'
     chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
-    gas: 9550000
+    gas: 500000
     fees: 15000000alnt
 EOF
 
